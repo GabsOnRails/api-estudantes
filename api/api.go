024f47d83af0a3fe07f92e63aae30832bd687b25
Gabriel <gabrielfelipe.oliveira2000@gmail.com>
2026-03-31 10:55:47 -0300

    separating handlers

--- api/api.go
+++ api/api.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"net/http"
-	"strconv"
 	"sync"
 
 	"github.com/GabsOnRails/api-estudantes/db"
@@ -60,62 +58,4 @@ func (api *API) ConfigureRoutes() {
 	api.Echo.GET("/students/:id", api.getStudent)
 	api.Echo.PUT("/students/:id", api.updateStudent)
 	api.Echo.DELETE("/students/:id", api.deleteStudent)
-}
-
-//----------
-// Handlers
-//----------
-
-func (api *API) createStudent(c *echo.Context) error {
-	student := db.Student{}
-
-	if err := c.Bind(&student); err != nil {
-		return err
-	}
-
-	err := api.DB.AddStudent(student)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{
-			"error": "Error to create student",
-		})
-	}
-
-	return c.JSON(http.StatusCreated, student)
-}
-
-func (api *API) getStudent(c *echo.Context) error {
-	lock.Lock()
-	defer lock.Unlock()
-	id, _ := strconv.Atoi(c.Param("id"))
-	return c.JSON(http.StatusOK, students[id])
-}
-
-func (api *API) updateStudent(c *echo.Context) error {
-	lock.Lock()
-	defer lock.Unlock()
-	u := new(user)
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	id, _ := strconv.Atoi(c.Param("id"))
-	students[id].Name = u.Name
-	return c.JSON(http.StatusOK, students[id])
-}
-
-func (api *API) deleteStudent(c *echo.Context) error {
-	lock.Lock()
-	defer lock.Unlock()
-	id, _ := strconv.Atoi(c.Param("id"))
-	delete(students, id)
-	return c.NoContent(http.StatusNoContent)
-}
-
-func (api *API) getStudents(c *echo.Context) error {
-	students, err := api.DB.GetStudents()
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{
-			"error": "Error to get students",
-		})
-	}
-	return c.JSON(http.StatusOK, students)
 }
